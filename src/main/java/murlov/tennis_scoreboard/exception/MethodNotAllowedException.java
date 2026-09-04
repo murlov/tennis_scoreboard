44cc6240ad82d75ee6ad15e343578853d5275e29
murlov <amolovv@gmail.com>
2026-09-05 01:40:03 +0300

    feat: add MethodNotAllowedException

---
+++ src/main/java/murlov/tennis_scoreboard/exception/MethodNotAllowedException.java
@@ -0,0 +1,7 @@
+package murlov.tennis_scoreboard.exception;
+
+public class MethodNotAllowedException extends RuntimeException {
+    public MethodNotAllowedException(String message) {
+        super(message);
+    }
+}
