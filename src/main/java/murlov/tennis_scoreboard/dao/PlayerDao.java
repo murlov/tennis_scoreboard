47938e966a38d0f858057ee72ca8e56320401b65
murlov <amolovv@gmail.com>
2026-09-08 05:00:23 +0300

    feat: add save() to PlayerDao

--- src/main/java/murlov/tennis_scoreboard/dao/PlayerDao.java
+++ src/main/java/murlov/tennis_scoreboard/dao/PlayerDao.java
@@ -1,8 +1,11 @@
 package murlov.tennis_scoreboard.dao;
 
+import murlov.tennis_scoreboard.exception.DuplicateException;
 import murlov.tennis_scoreboard.model.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 
 import java.util.Optional;
 
@@ -22,6 +25,23 @@ public final class PlayerDao {
                     )
                     .setParameter("name", name)
                     .uniqueResultOptional();
+        }
+    }
+
+    public Player save(String name) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            Player player = new Player(name);
+            session.persist(player);
+
+            transaction.commit();
+
+            return player;
+        } catch (ConstraintViolationException e) {
+            throw new DuplicateException(
+                    "Player with name '" + name + "' already exists"
+            );
         }
     }
 }
