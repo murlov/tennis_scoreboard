50138684a2f72f7c7882482e8874f7a21ffaf386
murlov <amolovv@gmail.com>
2026-09-13 13:55:08 +0300

    feat: add ValidationException

---
+++ src/main/java/murlov/tennis_scoreboard/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package murlov.tennis_scoreboard.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
