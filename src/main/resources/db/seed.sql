bdf219feee0450973d191eda6bc021848e16efd6
murlov <amolovv@gmail.com>
2026-09-20 18:54:21 +0300

    chore: remove unused schema.sql and seed.sql

--- src/main/resources/db/seed.sql
+++
@@ -1,9 +0,0 @@
-insert into players (name)
-values ('Petr'),
-       ('Alex'),
-       ('Ivan'),
-       ('Sergey'),
-       ('Vlad'),
-       ('Michael'),
-       ('Max')
-on conflict (name) do nothing;
